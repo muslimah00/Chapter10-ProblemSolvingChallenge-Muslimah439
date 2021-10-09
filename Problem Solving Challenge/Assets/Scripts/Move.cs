44ca1d650600a64acd88c5c3d6389b3667df4a3f
muslimah00 <muslimah.mbs@gmail.com>
2021-10-10 00:06:31 +0700

    Menyebar kotak
    Sebarkan kotak-kotak kecil dalam kotak secara random baik jumlah maupun letaknya.

---
+++ Problem Solving Challenge/Assets/Scripts/Move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    float vertical, horizontal;
+    Rigidbody2D myRigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBall();
+    }
+
+    void MoveBall()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        myRigidbody2D.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+    }
+}
