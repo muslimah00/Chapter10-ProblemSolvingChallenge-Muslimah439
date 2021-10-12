8eec1fd29e3064457081f3a279ec6103a6a3bca8
muslimah00 <muslimah.mbs@gmail.com>
2021-10-12 22:28:02 +0700

    Adding gameover and timer
    Kembangkan hasil pekerjaan menjadi sebuah game sebisa kalian, pastikan kalian juga memikirkan hal-hal yang bisa membuat sebuah game yang bagus, bukan hanya spesifikasi teknis yang diberikan.

---
+++ Problem Solving Challenge/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    private int AmbilScore = 0;
+
+    public float AmbilDataScore()
+    {
+        return AmbilScore;
+    }
+
+    public void TambahScore (int increment)
+    {
+        AmbilScore += increment;
+    }
+}
---
+++ Problem Solving Challenge/Assets/Scripts/Square.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+ 
+public class Square : MonoBehaviour
+{
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+ 
+ 
+    void Start ()
+    {
+        //Mengeksekusi fungs Spawn setiap beberapa detik sesui dengan nilai spawnTime
+        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+        //Spawn ();
+    }
+ 
+ 
+    void Spawn ()
+    {
+        Vector3 posisitionSquare = new Vector3(Random.Range(-10f, 10f), Random.Range(-3f, 3f), 0f);
+
+        //Mendapatkan nilai random
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+ 
+        //Memduplikasi enemy
+        Instantiate (enemy, posisitionSquare, spawnPoints[spawnPointIndex].rotation);
+    }
+}
