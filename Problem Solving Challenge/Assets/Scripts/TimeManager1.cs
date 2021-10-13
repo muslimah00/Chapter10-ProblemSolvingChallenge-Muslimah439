48e3c880b545569f9c8d779a932847ce1fb646f6
muslimah00 <muslimah.mbs@gmail.com>
2021-10-13 22:54:43 +0700

    Menu Selection
    Buat satu build berisi menu selection untuk mengakses semua build.

---
+++ Problem Solving Challenge/Assets/Scripts/TimeManager1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager1 : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    [SerializeField] Text timerText;
+    [SerializeField] float duration, currentTime;
+
+    void Start()
+    {
+        panel.SetActive(false);
+        currentTime = duration;
+        timerText.text = currentTime.ToString();
+        StartCoroutine(TimeIEn());
+    }
+
+    IEnumerator TimeIEn()
+    {
+        while(currentTime >= 0)
+        {
+            timerText.text = currentTime.ToString();
+            yield return new WaitForSeconds(1f);
+            currentTime--;
+        }
+        OpenPanel();
+    }
+
+    void OpenPanel()
+    {
+        timerText.text = "";
+        panel.SetActive(true);
+    }
+}
